95c0c896ca41572f1bc1a8dcbe6115063ed37771
ChaseChoi <caiguihao@gmail.com>
2018-01-12 12:52:56 +0800

    finish

--- ElGamal.c
+++ ElGamal.c
@@ -28,14 +28,19 @@ void findGenerator(mpz_t generator, mpz_t prime) {
 }
 
 int main() {
-	mpz_t prime, generator, publicKeyOfA, privateKeyOfA;
-     // init state
+	mpz_t prime, generator, publicKeyOfA, privateKeyOfA, privateKeyOfB, maskingKeyOfB, ephemeralKey, cipher, maskOfA, invert_maskOfA, temp, decrypted;
+	mpz_t msg;
+	char* plain = "123456789";
+
+     // init random state
     gmp_randinit_mt(state);
     gmp_randseed_ui(state, time(NULL));
-    mpz_inits(privateKeyOfA, publicKeyOfA, NULL);
+    // init var
+    mpz_inits(privateKeyOfA, publicKeyOfA, privateKeyOfB, maskingKeyOfB, ephemeralKey, msg, cipher, maskOfA, invert_maskOfA, temp, decrypted, NULL);
+   	// define precision
    	const unsigned long int bits = 1023;
     
-   	// A part 
+   	// A part: key gen
     randPrime(prime, bits);
     gmp_printf("===prime===\n%Zd\n", prime);
     findGenerator(generator, prime);
@@ -46,10 +51,24 @@ int main() {
 
 	gmp_printf("===publicKeyOfA===\n%Zd\n", publicKeyOfA);
 
-	// B part
-	
+	// B part: send msg to A
+	mpz_urandomm(privateKeyOfB, state, prime);
+	mpz_powm(ephemeralKey, generator, privateKeyOfB, prime);  // cal ephemeral key: generator^{i} mod prime
+	mpz_powm(maskingKeyOfB, publicKeyOfA, privateKeyOfB, prime);
+	// encrypt msg
+	mpz_init_set_str(msg, plain, 10);
+
+	mpz_mul(cipher, msg, maskingKeyOfB);
+    mpz_mod(cipher, cipher, prime);
+
+	// A part: recv cipher from B
+    mpz_powm(maskOfA, ephemeralKey, privateKeyOfA, prime);
+    mpz_invert(invert_maskOfA, maskOfA, prime);
+    mpz_mul(temp, invert_maskOfA, cipher);
+    mpz_mod(decrypted, temp, prime);
+    gmp_printf("===Plain text: ===\n%Zd\n", decrypted);
 
 	// clean up
-	mpz_clears(privateKeyOfA, publicKeyOfA, generator, prime, NULL);
+	mpz_clears(privateKeyOfA, publicKeyOfA, generator, prime, privateKeyOfB, maskingKeyOfB, ephemeralKey, cipher, maskOfA, invert_maskOfA, temp, decrypted, NULL);
     return 0;
 }
